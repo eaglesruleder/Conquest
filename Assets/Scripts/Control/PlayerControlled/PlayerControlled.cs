912fa6a18e1f529fb0c5d3e2ed7d9b93f14624e9
Dillon <dillon.odwyer@smsmining.com>
2022-01-14 19:31:46 +0800

    PlayerControlled Method Cleanup

--- Assets/Scripts/Control/PlayerControlled/PlayerControlled.cs
+++ Assets/Scripts/Control/PlayerControlled/PlayerControlled.cs
@@ -2,8 +2,8 @@
 
 using OdWyer.Control;
 
-public abstract class PlayerControlled : MonoBehaviour {
-
+public abstract class PlayerControlled : MonoBehaviour
+{
 	public abstract string Name { get; }
     public MeshHandler SelectionObj;
 
@@ -15,27 +15,21 @@ public abstract class PlayerControlled : MonoBehaviour {
 	internal float supplyDrained = 0;
 	public int CurrentSupply{ get{return (supplyDrained < Supply) ? Supply - (int)supplyDrained : 0;}}
 
-	// Instantiated Vairables
     public string playerID;
 
-    public virtual void SetPlayer(string PlayerID)
-    {
-		playerID = PlayerID;
-    }
+    public virtual void SetPlayer(string PlayerID) => playerID = PlayerID;
 
     public abstract bool Damage(int damage, float[] armorBonus, Vector3 hitPoint);
 
     public void Selected(bool Select)
     {
         if (SelectionObj)
-        {
             SelectionObj.gameObject.SetActive(Select);
-        }
     }
 
     public virtual void EndSelf()
     {
-		OnBecameInvisible ();
+		OnBecameInvisible();
 		Selected (false);
         if(!IsInvoking("EndNow"))
         {
@@ -47,19 +41,11 @@ public abstract class PlayerControlled : MonoBehaviour {
         Destroy(gameObject);
     }
 
-	// Add pc to list of OnScreen selectables via Renderer
-    public void OnBecameInvisible()
-    {
-		PCSelector.OffScreen(this);
-    }
-
-	// Remove pc from list of OnScreen selectables via Renderer
+    public void OnBecameInvisible() => PCSelector.OffScreen(this);
     public void OnBecameVisible()
     {
 		if(CurrentHealth > 0)
-        {
 			PCSelector.OnScreen(this);
-        }
     }
 
     public abstract void SetTarget(PlayerControlled Target);
